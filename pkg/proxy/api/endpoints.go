bee71feb89f0ae3f2302d664327a82a519d95358
MadhurNehete <132289905+MadhurNehete@users.noreply.github.com>
2026-05-15 13:08:14 +0530

    fix: resolve routing 404s and finalize simulator stabilization

--- pkg/proxy/api/endpoints.go
+++ pkg/proxy/api/endpoints.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"net/url"
 	"strconv"
+	"strings"
 	"sync/atomic"
 	"time"
 
@@ -88,6 +89,13 @@ func (ep *endpointsProcessor) ExtendProxyServer(httpServer *http.Server) error {
 	ws.POST(forceUpdateValidatorStatistics, ep.forceUpdateValidatorStatistics)
 	ws.GET(observersInfo, ep.getObserversInfo)
 	ws.POST(epochChange, ep.forceEpochChange)
+	ws.NoRoute(func(c *gin.Context) {
+		path := c.Request.URL.Path
+		if !strings.HasPrefix(path, "/v1.0/") {
+			c.Request.URL.Path = "/v1.0" + path
+			ws.HandleContext(c)
+		}
+	})
 
 	serializerForLogs := &marshal.GogoProtoMarshalizer{}
 	registerLoggerWsRoute(ws, serializerForLogs)
