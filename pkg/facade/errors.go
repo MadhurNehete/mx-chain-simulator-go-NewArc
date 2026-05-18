aa253c97302bd015857d9c64a04dd84af1f08ced
MadhurNehete <132289905+MadhurNehete@users.noreply.github.com>
2026-05-18 14:38:57 +0530

    fix: remove unused errInvalidNumOfBlocks and stop tracking large binaries

--- pkg/facade/errors.go
+++ pkg/facade/errors.go
@@ -4,6 +4,5 @@ import "errors"
 
 var (
 	errNilSimulatorHandler         = errors.New("nil simulator handler ")
-	errInvalidNumOfBlocks          = errors.New("num of blocks must be greater than zero")
 	errNilProxyTransactionsHandler = errors.New("nil proxy transactions handler ")
 )
